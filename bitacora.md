fc3b1d716f017022b80378041a44ad9441bdf2e4
Laura Urrea <lauramilena.99.10@gmail.com>
2025-02-28 20:10:55 -0500

    v0.3

--- bitacora.md
+++ bitacora.md
@@ -13,3 +13,15 @@ Crear una página que muestre un mapa centrado en mi barrio
 
 # solución propuesta
 Utilizar una biblioteca de visor geográfico: leaflet, openlayers, mapopen
+
+# problema 4 
+
+Mostrar el polígono de mi barrio 
+# solucion
+buscar geojson de terceros 
+cargar el geoJson usando async away
+
+# problema 5 
+
+crear un boton y asignarle un manejador de eventos 
+
--- program.js
+++ program.js
@@ -1,5 +1,22 @@
-var map = L.map('map').setView([4.570341350278083, -74.14461308243789], 13);
+var map = L.map('map').setView([4.570341350278083, -74.14461308243789], 17);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+async function loadPolygon() {  
+    let myData = await fetch('chircalsur.geojson');
+    let myPolygon = await myData.json();
+    
+    L.geoJSON(myPolygon, {
+        style: {
+            color: "blue"
+        }
+    }).addTo(map);
+}
+
+loadPolygon();
+
+let btnTress =document.getElementById('btnTress');
+
+btnTress.addEventListener('click', ()=> alert("hola"));
