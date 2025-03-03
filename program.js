043c0a189a652723d3f1194d88d2bacd4c8ce10c
Eliana <sigmorab@gmail.com>
2025-03-02 22:28:26 -0500

    Corrección de errores en la generación de PDF y filtrado de siniestros

--- program.js
+++ program.js
@@ -117,7 +117,7 @@ btnDistance.addEventListener('click',
 
                         )
                         }
-                    }
+                    } 
                 ) 
             }
             )
